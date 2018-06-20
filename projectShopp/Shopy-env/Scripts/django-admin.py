629b3f2289fe00a54487ae5456e238ed853e9ee6
LissC <liseth22794ccl@gmail.com>
2018-06-20 03:58:21 -0400

    //

--- projectShopp/Shopy-env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\liscl\documents\python\projectdjango\shopy\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
